5d8bff0702e23aee364b8d713caba2fe68ec9df5
KingOris <47605407+KingOris@users.noreply.github.com>
2019-02-20 21:06:35 -0600

    Made some change for listing guild

--- bot2.py
+++ bot2.py
@@ -1,5 +1,10 @@
 from discord.ext.commands import Bot
-
+"""""
+@Created by Yuhang chen(Kingoris)
+@Date: 2/20/2019
+@Discription:
+    This bot is for record hunting people in Black Desert online
+"""""
 Bot_Prefix = ("?", "~")
 TOKEN = "NTM4NDY1Mzc5NjY0NTI3NDEx.Dy1zeQ.3FwlKeRU5Am1bS4MqmKH-gxpfAc"
 
@@ -13,49 +18,45 @@ while gnames:
     gnames = g_l_file.readline()
 g_l_file.close()
 
-"""""
-@client.command(name='hunting',
-                description="Show bounty hunting list",
-                brief="Show hunting list"
-                )
-#async def hunting():
-    for HuntingDic in listHunting:
-        await client.say('Guild' + ' ' + HuntingDic.guild + ' ' + 'CharacterName' + ' ' + HuntingDic.name)
-"""""
 
-
-@client.command()
+@client.command(name='adding',
+                description="Adding [guild] [name] for hunting",
+                brief="Adding [guild] [name]")
 async def adding(guild, name):
     add_hunt_list(guild, name)
     await client.say(
         'Guild' + ' ' + guild + ' ' + 'CharacterName' + ' ' + name + ' has been added')
 
 
-@client.command()
+@client.command(name='listing',
+                description="List all data",
+                brief="List all data")
 async def listing():
-    for guildName in guild_name_list:
-        await client.say("Guild: " + guildName)
-        file = open(guildName, "r")
-        try:
-            names = file.readline()
-            while names:
-                if names != " ":
-                    await client.say(names)
-                    names = file.readline()
-        except IOError:
-            await client.say("No hunting list")
-    file.close()
+    if len(guild_name_list) != 0:
+        for guildName in guild_name_list:
+            await client.say("Guild: " + guildName)
+            file = open(guildName, "r")
+            try:
+                names = file.readline()
+                while names:
+                    if names != " ":
+                        await client.say("-"+names)
+                        names = file.readline()
+            except IOError:
+                await client.say("No hunting list")
+        file.close()
+    else:
+        await client.say("No hunting list")
+
 
 def add_hunt_list(guildName, name):
-    try:
-        file = open(guildName, "a")
-        file.write(name + "\n")
-        file.close()
-        guild_name_list.append(guildName)
-    except IOError:
-        newFile = open(guildName, "+")
-        newFile.write(name + "\n")
-        newFile.close()
+    file = open(guildName, "a")
+    file.write(name + "\n")
+    file.close()
+    guild_name_list.append(guildName)
+    guild_name = open("GuildName", "a")
+    guild_name.write(guildName + "\n")
+    guild_name.close()
 
 
 client.run(TOKEN)
